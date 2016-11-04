f79a2124f4c225c5e3091751a0d1c775a74b5655
liying <liying@bblink.cn>
2016-11-04 16:10:29 +0800

    添加ip及数据库存储

--- server.js
+++ server.js
@@ -13,11 +13,41 @@
     var messages=[];
     //socket部分
     io.on('connection', function(socket) {
+        socket.on('messages', function(data) {
+            if(messages.length<=0){
+                db.open(function(err, db){
+                    if(!err){
+                        db.createCollection('message', {safe:true}, function(err, collection){
+                            if(err){
+                                console.log(err);
+                            }else{
+                                collection.find({"ip":{$ne:null}}).sort({"time":-1}).toArray(function(err,docs){
+                                  // console.log('find');
+                                   //console.log(docs);
+                                   docs.forEach( function(element, index) {
+                                       messages.push(element);
+                                   });
+                                   db.close();
+                                }); 
+
+                            }
+                        });
+
+                    }else{
+                        console.log(err);
+                    }
+                });
+            }
+            socket.emit("messages", messages,socket.handshake.address);
+        });
         //接收并处理客户端发送的foo事件
         socket.on('message', function(data) {
             //将消息输出到控制台
-            console.log(data);
-            var message={"name":"jetty","message":data};
+            //console.log(data);
+            var today=new Date();
+            var time=today.getFullYear()+"-"+today.getMonth()+"-"+today.getDate()+" "+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+            var message={"ip":socket.handshake.address,"message":data,"time":time};
+            messages.push(message);
             db.open(function(err, db){
                 if(!err){
                     db.createCollection('message', {safe:true}, function(err, collection){
@@ -25,14 +55,9 @@
                             console.log(err);
                         }else{
                             collection.insert(message,{safe:true},function(err,result){
-                                console.log(result);
+                               // console.log(result);
+                                db.close();
                             });
-                            collection.find().toArray(function(err,docs){
-                               console.log('find');
-                               console.log(docs);
-                               messages=docs;
-                               db.close();
-                            }); 
 
                         }
                     });
@@ -42,13 +67,13 @@
                 }
             });
            // db.close();
-            socket.broadcast.emit("message", data, 'left');
-            socket.emit("message", data, 'right');
+            socket.broadcast.emit("message", message, 'left');
+            socket.emit("message", message, 'right');
         })
     });
-     db.on("close", function (err,db) {//关闭数据库
-     if(err) throw err;
-     else console.log("成功关闭数据库.");
- });
+    db.on("close", function (err,db) {//关闭数据库
+        if(err) throw err;
+        else console.log("成功关闭数据库.");
+    });
    
     
